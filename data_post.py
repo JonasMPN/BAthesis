92e5120f4891440e9ade64da0807d2dbe9031f03
Jonas <jonas.mpn@gmail.com>
2022-04-20 20:07:21 +0200

    Changes
    Changed:
    - Way each category is plotted. The colours can now be directly assigned.

--- data_post.py
+++ data_post.py
@@ -1,4 +1,4 @@
-import helper_functions as hf
+from helper_functions import Helper
 from typing import List
 from matplotlib import pyplot as plt
 import pandas.plotting as pplot
@@ -8,7 +8,7 @@ import pandas as pd
 import pandas.errors
 import numpy as np
 from copy import copy
-
+helper = Helper()
 
 class PostVisualisation():
     def __init__(self, dir_results: str, protocol_file: str, criteria_column: str, single_params_file: str,
@@ -16,9 +16,10 @@ class PostVisualisation():
         cols_to_ignore = [] if columns_to_ignore is None else columns_to_ignore
         self.dir_results = dir_results
         self.criteria_col = criteria_column
-        hf.create_directory(self.dir_results)
+        helper.create_dir(self.dir_results)
         try:
             self.df = pd.read_csv(protocol_file, index_col=None).drop(columns=cols_to_ignore)
+            self.df = self.df.loc[self.df["nVortices"] == "[1]"]
             if "None" in self.df[self.criteria_col].unique():
                 max_distance = self.df[self.criteria_col].loc[self.df[self.criteria_col] != "None"].max()
                 self.df[self.criteria_col] = self.df[self.criteria_col].replace({"None": max_distance})
@@ -36,27 +37,33 @@ class PostVisualisation():
             self.df_single_params.to_csv(single_params_file, index=False)
         if self.df_single_params is None:
             raise LookupError("Could not create or use an existing file for the single parameters.")
+        self._background_category = "_background"  # colour lines w/o category in parallel coordinates
 
-
-        self._background_category = "_background"  # no reason to change this as this stays in the script
-
-    def parallel_coordinates(self, categories: dict, normalise_except: list[str]=None,  show_avg_categories=True,
-                             colours: list=None):
+    def parallel_coordinates(self, categories: dict, show_avg_categories=True):
         self.__clean_df()
         self.__translate_df()
         self.__categorise(categories)
-        self.__normalise_df(["species"] if normalise_except is None else ["species"]+normalise_except)
-        df_background = self.df.loc[self.df["species"] == self._background_category]
-        df_wanted = self.df.loc[self.df["species"] != self._background_category]
-        alpha_wanted = 0.2 if show_avg_categories else 1
+        self.__normalise_df(["species"])
+        plot_dfs = self.__get_plot_df((categories|{self._background_category:[0,0,"grey"]}), 0.2 if show_avg_categories
+                                       else 1)
         fig, ax = plt.subplots()
         fig.set_size_inches(16,9)
-        pplot.parallel_coordinates(ax=ax, frame=df_background, class_column="species", color="grey", alpha=0.05)
-        pplot.parallel_coordinates(ax=ax, frame=df_wanted, class_column="species", color=colours, alpha=alpha_wanted)
+
+        for df_info in plot_dfs:
+            df, colour, alpha = df_info["df"], df_info["colour"], df_info["alpha"]
+            pplot.parallel_coordinates(ax=ax, frame=df, class_column="species", color=colour, alpha=alpha)
         if show_avg_categories:
-            self.__plot_avg_categories(ax=ax, df=df_wanted, categories=categories, colours=colours)
-        plt.title(f"Unchanged params: {self.unchanged_params} \n"
-                  f"{self.criteria_col}=[{np.round(self.criteria_min, 3)}, {np.round(self.criteria_max, 3)}]")
+            for df_info in plot_dfs[1:]: # the first one is the background
+                df, colour, alpha = df_info["df"], df_info["colour"], df_info["alpha"]
+                df_averaged = pd.DataFrame()
+                for col in df.columns.tolist():
+                    if col == "species":
+                        df_averaged["species"] = f"_{df[col].iloc[0]}"
+                    else:
+                        df_averaged[col] = [df[col].mean()]
+                pplot.parallel_coordinates(ax=ax, frame=df_averaged, class_column="species", color=colour, linewidth=3)
+        plt.title(f"{self.criteria_col}=[{np.round(self.criteria_min, 3)}, {np.round(self.criteria_max, 3)}]")
+        plt.xticks(rotation="vertical")
         plt.savefig(self.dir_results+"/parallel_coordinates.png", dpi=180, bbox_inches="tight")
         plt.cla()
         plt.clf()
@@ -85,13 +92,31 @@ class PostVisualisation():
         plt.savefig(self.dir_results+f"/{parameter}.png", dpi=180, bbox_inches="tight")
         plt.cla(), plt.clf()
 
+    def __get_plot_df(self, categories: dict, non_background_alpha: float) -> List[dict]:
+        df_single_species = list()
+        species = [self._background_category] + [col for col in self.df["species"].unique() if col != self._background_category]
+        for species in species:
+            df_single_species.append({"df": self.df.loc[self.df["species"] == species],
+                                      "colour": categories[species][2],
+                                      "alpha": non_background_alpha if species != self._background_category else 0.05})
+        return df_single_species
+
     def __clean_df(self):
+        """
+        Drops parameters (columns) that have a single fixed value throughout the protocol.
+        :return:
+        """
         for col in copy(self.df.columns).tolist():
-            if len(self.df[col].unique()) == 1:
+            if len(self.df[col].unique()) == 1 or "dir" in col:
                 self.df = self.df.drop([col], axis=1)
                 self.unchanged_params.append(col)
 
     def __translate_df(self):
+        """
+        Translates parameter values that are not floats into ascending numbers. These numbers will be saved to an
+        additional file in the result_dir.
+        :return:
+        """
         dtypes = dict(self.df.dtypes)
         translation = list()
         for col, dtype in dtypes.items():
@@ -112,6 +137,11 @@ class PostVisualisation():
             pd.DataFrame(df_translation).to_csv(self.dir_results+"/translation.dat", index=False)
 
     def __categorise(self, categories: dict):
+        """
+        This function categorises the criteria column into the categories of 'categories'.
+        :param categories:
+        :return:
+        """
         self.criteria_min, self.criteria_max = self.df[self.criteria_col].min(), self.df[self.criteria_col].max()
         categories = self.__fill_categories(categories)
         species_idx = {category: list() for category in categories.values()}
@@ -136,19 +166,24 @@ class PostVisualisation():
         self.df = self.df.sort_values("species", ascending=False)
 
     def __normalise_df(self, cols_to_ignore: list):
+        """
+        Normalises the columns of a dataframe.
+        :param cols_to_ignore:
+        :return:
+        """
         cols_to_normalise = [col for col in self.df.columns.tolist() if col not in cols_to_ignore]
         for col in cols_to_normalise:
             self.df[col] = self.df[col]/self.df[col].max()
 
-    def __plot_avg_categories(self, ax: matplotlib.axes.Axes, df: pd.DataFrame, categories: dict[str:list],
-                              colours: list=None):
+    def __plot_avg_categories(self, ax: matplotlib.axes.Axes, df: pd.DataFrame, categories: dict[str:list]):
         cols_to_mean = [col for col in df.columns.tolist() if df[col].dtype != "object"]
-        for category, colour in zip(categories.keys(), colours):
+        for category, category_info in categories.items():
             df_categorised = df.loc[df["species"] == category]
             df_averaged = pd.DataFrame({"species": [f"_{category}"]})
             for col in cols_to_mean:
                 df_averaged[col] = df_categorised[col].mean()
-            pplot.parallel_coordinates(ax=ax, frame=df_averaged, class_column="species", color=colour, linewidth=3)
+            pplot.parallel_coordinates(ax=ax, frame=df_averaged, class_column="species", color=category_info[2],
+                                       linewidth=3)
 
     def __plot_single_param(self, parameter: str, criteria_col:str, amount: int or None):
         df_iter = self.df_single_params[self.df_single_params["parameter"] == parameter]
@@ -172,7 +207,8 @@ class PostVisualisation():
         plt.cla(), plt.clf()
 
     def __fill_categories(self, categories: dict[str:list]) -> dict:
-        """Returns a dict. The keys are the percentage up to which the category (value) applies."""
+        """Returns a dict. The keys are the percentage up to which the category (value) applies. The supplied
+        categories need to be ascending!"""
         last_value = 0
         complete_categories = dict()
         for id, (category, interval) in enumerate(categories.items()):
